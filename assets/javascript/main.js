2f023dd430e47b85b8ce7d46c86e6e3d65d5083f
John Lewis <jalewis1221@gmail.com>
2018-07-22 16:06:08 -0500

    working the foursquare integration

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -22,8 +22,21 @@ $("#submit").on("click", function() {
 
   // Here we are building the URL we need to query the database for OpenWeatherMaps
   var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput},USA${APIKey1}`;
-  var queryURL2 = `https://api.foursquare.com/v2/venues/explore?near=${userInput}&limit=10&section=topPicks&client_id=${clientID}&client_secret=${APIKey2}&v=20180721`; 
+  var queryURL2 = `https://api.foursquare.com/v2/venues/explore?near=${userInput}&limit=10&venuePhotos=1&section=topPicks&client_id=${clientID}&client_secret=${APIKey2}&v=20180721`; 
   // We then created an AJAX call
+ 
+
+  // <div class="row">
+  //               <div class="col-md-12 text-center">
+  //                   <div id="pic"></div>
+  //                   <p>Venue 1: <span id="venue"></span></p>
+  //                   <p>Location: <span id="location"></span></p>
+  //                   <p>Category: <span id="category"></span></p>
+                    
+                    
+  //               </div>
+  //           </div>
+
 
 
   $.ajax({
@@ -32,6 +45,50 @@ $("#submit").on("click", function() {
   }).then(function(results) {
     console.log(queryURL2);
     console.log(results.response.groups[0].items[0].venue.name);
+    console.log(results);
+    let items = results.response.groups[0].items
+    for (let i = 0; i < items.length; i++) {
+    //dynamically creates the html with our ajax response
+    let venue = items[i].venue;
+  
+    
+    let $row = $("<div>").addClass("row venue");
+    let $col = $("<div>").addClass("col-md-12 text-center").attr("id", `venue${i}`);
+    let $name = $("<p>").text(`Venue ${i + 1}: ${venue.name}`);
+    let $location = $("<p>").text(`Location: ${venue.location.address}`);
+    let $category = $("<p>").text(`Category: ${venue.categories[0].name}`);
+    
+    
+
+
+
+
+
+  
+   
+
+    //fetching pictures for each venue with another ajax call inside an ajax call
+    $col.append($name,$location,$category).appendTo($row);
+    $("#topPicks").append($row);
+
+    
+    $.ajax({
+      url: `https://api.foursquare.com/v2/venues/${venue.id}/photos?client_id=${clientID}&client_secret=${APIKey2}&v=20180721`,
+      method: "GET"
+    }).then(function(picRes) {
+      console.log("results", picRes);
+      let photo = picRes.response.photos.items[0];
+      let  $pic = $("<div>").html(`<img src="${photo.prefix}200x200${photo.suffix}">`);
+      $(`#venue${i}`).prepend($pic);
+      console.log($(`#venue[${i}]`));
+      
+      
+    });
+   
+      
+    
+  }
+    
   });
 
   $.ajax({
@@ -41,8 +98,9 @@ $("#submit").on("click", function() {
     console.log(queryURL);
     console.log(response.list[0].dt_txt);
     console.log(moment(response.list[0].dt_txt ).format('l'));
+    console.log(response);
   })
-
+ 
 
 
 
