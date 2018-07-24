ddd809585d4f622ce70e37552d26b625b70ad4db
John Lewis <jalewis1221@gmail.com>
2018-07-23 22:15:38 -0500

    I updated the foursquare results with two photos per venue

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,6 +14,10 @@ $("#submit").on("click", function() {
     .val()
     .trim();
   console.log(userInput);
+  //This clears out the search bar on-click
+  $("#cityInput").val("");
+  //This clears the previous foursquare results before the next search renders
+  $("#topPicks").empty();
 
   // This is our API keys
   var APIKey1 = "&APPID=43c7b0d44b7655fca26cc7c25917a267";
@@ -23,22 +27,8 @@ $("#submit").on("click", function() {
   // Here we are building the URL we need to query the database for OpenWeatherMaps
   var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput},USA${APIKey1}`;
   var queryURL2 = `https://api.foursquare.com/v2/venues/explore?near=${userInput}&limit=10&venuePhotos=1&section=topPicks&client_id=${clientID}&client_secret=${APIKey2}&v=20180721`; 
-  // We then created an AJAX call
  
-
-  // <div class="row">
-  //               <div class="col-md-12 text-center">
-  //                   <div id="pic"></div>
-  //                   <p>Venue 1: <span id="venue"></span></p>
-  //                   <p>Location: <span id="location"></span></p>
-  //                   <p>Category: <span id="category"></span></p>
-                    
-                    
-  //               </div>
-  //           </div>
-
-
-
+  //This is out ajax call for foursquare
   $.ajax({
     url: queryURL2,
     method: "GET"
@@ -46,51 +36,57 @@ $("#submit").on("click", function() {
     console.log(queryURL2);
     console.log(results.response.groups[0].items[0].venue.name);
     console.log(results);
+    //This appends the header
+    $("#city-4sq").html("Whats hot in " + results.response.headerLocation + "!");
+    console.log(results.response.headerLocation)
+
     let items = results.response.groups[0].items
     for (let i = 0; i < items.length; i++) {
-    //dynamically creates the html with our ajax response
-    let venue = items[i].venue;
-  
-    
-    let $row = $("<div>").addClass("row venue");
-    let $col = $("<div>").addClass("col-md-12 text-center").attr("id", `venue${i}`);
-    let $name = $("<p>").text(`Venue ${i + 1}: ${venue.name}`);
-    let $location = $("<p>").text(`Location: ${venue.location.address}`);
-    let $category = $("<p>").text(`Category: ${venue.categories[0].name}`);
-    
-    
-
-
-
-
-
-  
-   
-
-    //fetching pictures for each venue with another ajax call inside an ajax call
-    $col.append($name,$location,$category).appendTo($row);
-    $("#topPicks").append($row);
-
-    
-    $.ajax({
-      url: `https://api.foursquare.com/v2/venues/${venue.id}/photos?client_id=${clientID}&client_secret=${APIKey2}&v=20180721`,
-      method: "GET"
-    }).then(function(picRes) {
-      console.log("results", picRes);
-      let photo = picRes.response.photos.items[0];
-      let  $pic = $("<div>").html(`<img src="${photo.prefix}200x200${photo.suffix}">`);
-      $(`#venue${i}`).prepend($pic);
-      console.log($(`#venue[${i}]`));
+        //dynamically creates the html with our ajax response
+        let venue = items[i].venue;
       
+        let $row = $("<div>").addClass("row venue");
+        let $col = $("<div>").addClass("col-md-12 text-center").attr("id", `venue${i}`);
+        let $name = $("<p>").text(`Venue ${i + 1}: ${venue.name}`);
+        let $location = $("<p>").text(`Location: ${venue.location.address}`);
+        let $category = $("<p>").text(`Category: ${venue.categories[0].name}`);
+
+        //This appends all the html elements we just created above in one simple chain into the topPicks div in our html
+        $col.append($name,$location,$category).appendTo($row);
+        $("#topPicks").append($row);
+        
+        
+        
+        
+        
+       
+
+        
+        
+        //fetching pictures for each venue with another ajax call inside an ajax call
+        $.ajax({
+          url: `https://api.foursquare.com/v2/venues/${venue.id}/photos?client_id=${clientID}&client_secret=${APIKey2}&v=20180721`,
+          method: "GET"
+        }).then(function(picRes) {
+          console.log("results", picRes);
+          let photo = picRes.response.photos.items;
+          for (let t = 0; t < photo.length; t++) {
+            
+            let  $pic = $("<div>").html(`<img src="${photo[t].prefix}200x200${photo[t].suffix}">`);
+            //This will append our photos to the div col with the id attr of venue{i}
+            // we had to give each class an id to append things in the right order because the second ajax call for the photos
+            // does not happen at the same time as the first ajax call for the venue information
+            $(`#venue${i}`).append($pic);
+            console.log($(`#venue${i}`));
+        
+          }
+          
+        }); 
       
-    });
-   
-      
-    
-  }
+    }
     
   });
-
+  //This is our openWeatherMaps call
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -104,4 +100,47 @@ $("#submit").on("click", function() {
 
 
 
-});
+});//This is the end of our on-click function, everything is created inside of this
+
+
+
+
+
+
+
+//     //fetching pictures for each venue with another ajax call inside an ajax call
+//     $.ajax({
+//       url: `https://api.foursquare.com/v2/venues/${venue.id}/photos?client_id=${clientID}&client_secret=${APIKey2}&v=20180721`,
+//       method: "GET"
+//     }).then(function(picRes) {
+//       console.log("results", picRes);
+//       let photo = picRes.response.photos.items[0];
+//       let  $pic = $("<div>").html(`<img src="${photo.prefix}200x200${photo.suffix}">`);
+//       //This will append our photos to the div col with the id attr of venue{i}
+//       // we had to give each class an id to append things in the right order because the second ajax call for the photos
+//       // does not happen at the same time as the first ajax call for the venue information
+//       $(`#venue${i}`).append($pic);
+//       console.log($(`#venue${i}`));
+//     }); 
+// }
+
+// });
+// //This is our openWeatherMaps call
+// $.ajax({
+// url: queryURL,
+// method: "GET"
+// }).then(function(response) {
+// console.log(queryURL);
+// console.log(response.list[0].dt_txt);
+// console.log(moment(response.list[0].dt_txt ).format('l'));
+// console.log(response);
+// })
+
+
+
+
+// });//This is the end of our on-click function, everything is created inside of this
+
+
+
+// 1 CommentCollapse 
